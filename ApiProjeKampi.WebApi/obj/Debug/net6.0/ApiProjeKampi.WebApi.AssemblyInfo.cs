28dfcecc14285a90d4cc7c758e367ef295100b19
aysebayrak <ayse.bayrak812@gmail.com>
2025-09-12 11:20:18 +0300

    Automapper işlemi

--- ApiProjeKampi.WebApi/obj/Debug/net6.0/ApiProjeKampi.WebApi.AssemblyInfo.cs
+++ ApiProjeKampi.WebApi/obj/Debug/net6.0/ApiProjeKampi.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiProjeKampi.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05df86846a5acfd472f49e68587db21bc1763be9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76b38134a43a78161c80be2a63b4c6c673746b23")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiProjeKampi.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiProjeKampi.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
