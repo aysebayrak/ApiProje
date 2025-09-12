a8b126bc55d980d10700b80c23586ccdb51f839b
aysebayrak <ayse.bayrak812@gmail.com>
2025-09-12 11:24:02 +0300

    Automapper işlemi

---
+++ ApiProjeKampi.WebApi/Controllers/MessagesController.cs
@@ -0,0 +1,16 @@
+﻿using ApiProjeKampi.WebApi.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly ApiContext  _context;
+
+       
+        
+    }
+}
